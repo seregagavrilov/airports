74b759ef34e2d3b3428b2a3a0f656ebeb8153317
Sergey Gavrilov <sergeigavrilov123@gmail.con>
2019-02-10 18:44:56 +0300

    add frontend react

--- rest_api_airports/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from rest_api_airports import models
-
-
-admin.site.register(models.Airport)
-admin.site.register(models.City)
-admin.site.register(models.Country)
-admin.site.register(models.UserCustom)
-
